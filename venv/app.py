c1355f65274a920a6d46218df17376bc93822372
SulaimanAhmedAbdullah <132942231+SulaimanAhmedAbdullah@users.noreply.github.com>
2024-05-08 21:22:26 +0300

    finished Gai

--- venv/app.py
+++ venv/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 import openai
 from flask_cors import CORS
-import os
 
 app = Flask(__name__)
 CORS(app)
@@ -10,12 +9,18 @@ OPENAI_API_KEY=""
 openai.api_key=OPENAI_API_KEY
 
 
+def words_to_tokens(word_count):
+    # Roughly estimate that 1 word is approximately 4 tokens
+    return int(word_count) * 4
+
+
 @app.route('/generate_story', methods=['POST'])
 def generate_story():
     try:
         # Get data from POST request
         data = request.json
 
+        max_tokens = words_to_tokens(data['words'])
         # Use the OpenAI API to generate the story
         response = openai.completions.create(
             model="gpt-3.5-turbo-instruct",  # or "text-davinci-002" based on the available engines at the time
@@ -24,6 +29,7 @@ def generate_story():
                    f"Characters: {', '.join(data['characters'])}\n"
                    f"Ending: {data['ending']}\n"
                    f"Write a story in {data['words']} words:",
+            max_tokens=max_tokens
         )
 
         # Extract the story from the API response
